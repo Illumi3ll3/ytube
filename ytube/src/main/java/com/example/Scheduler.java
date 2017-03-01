803e6b5b84ea93cc6064c7eea11cce06babe68f3
Illumi3ll3 <lu_mi_nos@yahoo.com>
2017-03-01 17:29:50 +0200

    f1 scheduler

---
+++ ytube/src/main/java/com/example/Scheduler.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Mihai
+ */
+@Component
+public class Scheduler {
+
+    private static final Logger log = LoggerFactory.getLogger (Scheduler.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat ("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println ("The time is now {}" + dateFormat.format (new Date ()));
+        log.info ("The time is now {}", dateFormat.format (new Date ()));
+    }
+
+}
